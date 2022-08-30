565d33b518592b085b2493501a5e93dbd7404c30
FaimaKhan <112518712+FaimaKhan@users.noreply.github.com>
2022-08-30 18:14:50 -0400

    Update streamlit_app.py
    Updated title and added more items to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title ('The Best Ever Bangladeshi Cuisine')
+streamlit.title ('Dosto's Tukitaki - The Best Ever Bangladeshi Cuisine')
 streamlit.title ('Breakfast Menu')
 streamlit.title ('Ruti')
 streamlit.title ('Alu bhaji')
-streamlit.title ('Lassi')
+streamlit.title ('Gorom Gorom Dudh Cha!!')

6d8aebfcfb06a1d06aadbe252402bf8bf92acbfb
FaimaKhan <112518712+FaimaKhan@users.noreply.github.com>
2022-08-30 18:10:34 -0400

    Update streamlit_app.py
    Modified title and also added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title ('Blah Blah and Blah Blah')
+streamlit.title ('The Best Ever Bangladeshi Cuisine')
+streamlit.title ('Breakfast Menu')
+streamlit.title ('Ruti')
+streamlit.title ('Alu bhaji')
+streamlit.title ('Lassi')

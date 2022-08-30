2803371146ad8477c732c20caaa06588b4403426
FaimaKhan <112518712+FaimaKhan@users.noreply.github.com>
2022-08-30 18:21:37 -0400

    Update streamlit_app.py
    Fixed formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title ('Dostos Tukitaki - The Best Ever Bangladeshi Cuisine')
-streamlit.title ('Breakfast Menu')
-streamlit.title ('Ruti')
-streamlit.title ('Alu bhaji')
-streamlit.title ('Gorom Gorom Dudh Cha!!')
+streamlit.header('Breakfast Menu')
+streamlit.text('Ruti')
+streamlit.text('Alu bhaji')
+streamlit.text('Gorom Gorom Dudh Cha!!')

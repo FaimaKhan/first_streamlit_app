f65baa3d6a2df57d9069982bc475b28e6f8bb6d0
FaimaKhan <112518712+FaimaKhan@users.noreply.github.com>
2022-08-30 18:44:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 streamlit.title ('Dostos Tukitaki - The Best Ever Bangladeshi Cuisine')
 streamlit.header('Breakfast Menu')
 streamlit.text('Ruti')
-streamlit.header('\N{flatbread} Ruti \N{flatbread}')
+streamlit.text(':flatbread: Ruti')
 streamlit.text('Alu bhaji')
 streamlit.text('Dim bhaji')
 streamlit.text('Gorom Gorom Dudh Cha!!')

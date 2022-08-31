80b8b692007dd57d69aaa21fd950ec3b1e7999ff
FaimaKhan <112518712+FaimaKhan@users.noreply.github.com>
2022-08-31 10:53:48 -0400

    Update streamlit_app.py
    Using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title ('Talk Of The Town - Homestyle Cooking')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥞Pancake🥞')
@@ -7,5 +8,7 @@ streamlit.text('🍳Eggs🍳')
 streamlit.text('🥤 Fresh Orange Juice 🥤')
 streamlit.text('🍌🥭🍑🍓Fruit Bowl 🍑🍓')
 streamlit.header('🥒 🥗 Make Your Own Smoothie 🍍🍌')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 

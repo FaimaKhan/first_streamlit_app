9c162bd3926c0bc952411a9220569614307100ff
FaimaKhan <112518712+FaimaKhan@users.noreply.github.com>
2022-09-02 14:06:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ try:
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-streamlit.error()
+   streamlit.error()
 #Don't run anything past here - trying to debug
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
